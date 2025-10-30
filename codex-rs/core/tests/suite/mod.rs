c6dc806cf1a574077001878cb0c257f572a1d5a7
Dylan Hurd <dylan.hurd@openai.com>
2025-10-29 22:40:40 -0700

    chore: testing on freeform apply_patch

--- codex-rs/core/tests/suite/mod.rs
+++ codex-rs/core/tests/suite/mod.rs
@@ -3,7 +3,11 @@
 #[cfg(not(target_os = "windows"))]
 mod abort_tasks;
 #[cfg(not(target_os = "windows"))]
+mod apply_patch;
+#[cfg(not(target_os = "windows"))]
 mod apply_patch_cli;
+#[cfg(not(target_os = "windows"))]
+mod apply_patch_freeform;
 #[cfg(not(target_os = "windows"))]
 mod approvals;
 mod cli_stream;
