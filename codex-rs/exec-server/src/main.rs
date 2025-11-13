74b071077d7fd8eb9771d8cb8021e79797510229
Jeremy Rose <nornagon@openai.com>
2025-11-13 12:03:02 -0800

    split

--- codex-rs/exec-server/src/main.rs
+++ codex-rs/exec-server/src/main.rs
@@ -7,7 +7,6 @@ use codex_core::exec::SandboxType;
 use codex_core::exec::process_exec_tool_call;
 use codex_core::get_platform_sandbox;
 use codex_core::protocol::SandboxPolicy;
-use libc::c_uint;
 use rmcp::ErrorData as McpError;
 use rmcp::RoleServer;
 use rmcp::ServerHandler;
@@ -24,20 +23,14 @@ use rmcp::transport::stdio;
 use serde::Deserialize;
 use serde::Serialize;
 use socket2::Domain;
-use socket2::MaybeUninitSlice;
-use socket2::MsgHdr;
-use socket2::MsgHdrMut;
 use socket2::Socket;
 use socket2::Type;
 use std::collections::HashMap;
 use std::io;
-use std::io::IoSlice;
-use std::mem::MaybeUninit;
 use std::os::fd::AsFd;
 use std::os::fd::AsRawFd;
 use std::os::fd::BorrowedFd;
 use std::os::fd::FromRawFd;
-use std::os::fd::OwnedFd;
 use std::os::fd::RawFd;
 use std::path::PathBuf;
 use std::process::Stdio;
@@ -48,13 +41,19 @@ use tokio::process::Command;
 use tracing_subscriber::EnvFilter;
 use tracing_subscriber::{self};
 
+use crate::socket::EscalateSocket;
+use crate::socket::receive_json_message;
+use crate::socket::send_json_message;
+
+mod socket;
+
 // C->S
 #[derive(Clone, Serialize, Deserialize, Debug)]
 enum ClientMessage {
     EscalateRequest {
         file: String,
         argv: Vec<String>,
-        workdir: String,
+        workdir: PathBuf,
     },
 }
 
@@ -80,121 +79,6 @@ struct SuperExecResult {
     exit_code: i32,
 }
 
-#[derive(Debug)]
-struct ReceivedMessage {
-    data: Vec<u8>,
-    #[allow(dead_code)]
-    fds: Vec<OwnedFd>,
-}
-
-fn assume_init(buf: &[MaybeUninit<u8>]) -> &[u8] {
-    unsafe { std::slice::from_raw_parts(buf.as_ptr().cast(), buf.len()) }
-}
-fn control_space_for_fds(count: usize) -> usize {
-    unsafe { libc::CMSG_SPACE((count * size_of::<RawFd>()) as _) as usize }
-}
-fn extract_fds(control: &mut [MaybeUninit<u8>], len: usize) -> std::io::Result<Vec<OwnedFd>> {
-    if len == 0 {
-        return Ok(Vec::new());
-    }
-    let mut fds = Vec::new();
-    let mut hdr: libc::msghdr = unsafe { std::mem::zeroed() };
-    hdr.msg_control = control.as_mut_ptr().cast();
-    hdr.msg_controllen = len as _;
-
-    let mut cmsg = unsafe { libc::CMSG_FIRSTHDR(&hdr) };
-    while !cmsg.is_null() {
-        let level = unsafe { (*cmsg).cmsg_level };
-        let ty = unsafe { (*cmsg).cmsg_type };
-        if level == libc::SOL_SOCKET && ty == libc::SCM_RIGHTS {
-            let data_ptr = unsafe { libc::CMSG_DATA(cmsg).cast::<RawFd>() };
-            let fd_count: usize = {
-                let cmsg_data_len =
-                    unsafe { (*cmsg).cmsg_len as usize } - unsafe { libc::CMSG_LEN(0) as usize };
-                cmsg_data_len / size_of::<RawFd>()
-            };
-            for i in 0..fd_count {
-                let fd = unsafe { data_ptr.add(i).read() };
-                fds.push(unsafe { OwnedFd::from_raw_fd(fd) });
-            }
-        }
-        cmsg = unsafe { libc::CMSG_NXTHDR(&hdr, cmsg) };
-    }
-    Ok(fds)
-}
-const MAX_FDS_PER_MESSAGE: usize = 16;
-const MAX_MESSAGE_SIZE: usize = 64 * 1024;
-fn receive_message(socket: &Socket) -> std::io::Result<ReceivedMessage> {
-    let mut data = [MaybeUninit::<u8>::uninit(); MAX_MESSAGE_SIZE];
-    let mut control = vec![MaybeUninit::<u8>::uninit(); control_space_for_fds(MAX_FDS_PER_MESSAGE)];
-    let (received, control_len) = {
-        let mut bufs = [MaybeUninitSlice::new(&mut data)];
-        let mut msg = MsgHdrMut::new()
-            .with_buffers(&mut bufs)
-            .with_control(&mut control);
-        let received = socket.recvmsg(&mut msg, 0)?;
-        (received, msg.control_len())
-    };
-
-    let message = assume_init(&data[..received]).to_vec();
-    let fds = extract_fds(&mut control, control_len)?;
-    Ok(ReceivedMessage { data: message, fds })
-}
-fn receive_json_message<T: for<'de> Deserialize<'de>>(
-    socket: &Socket,
-) -> std::io::Result<(T, Vec<OwnedFd>)> {
-    let ReceivedMessage { data, fds } = receive_message(socket)?;
-    let message: T = serde_json::from_slice(&data)?;
-    Ok((message, fds))
-}
-fn send_message_bytes(socket: &Socket, data: &[u8], fds: &[BorrowedFd<'_>]) -> std::io::Result<()> {
-    if fds.len() > MAX_FDS_PER_MESSAGE {
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::InvalidInput,
-            format!("too many fds: {}", fds.len()),
-        ));
-    }
-    let mut control = vec![0u8; control_space_for_fds(fds.len())];
-    unsafe {
-        let cmsg = control.as_mut_ptr().cast::<libc::cmsghdr>();
-        (*cmsg).cmsg_len = libc::CMSG_LEN(size_of::<RawFd>() as c_uint * fds.len() as c_uint) as _;
-        (*cmsg).cmsg_level = libc::SOL_SOCKET;
-        (*cmsg).cmsg_type = libc::SCM_RIGHTS;
-        let data_ptr = libc::CMSG_DATA(cmsg).cast::<RawFd>();
-        for (i, fd) in fds.iter().enumerate() {
-            data_ptr.add(i).write(fd.as_raw_fd());
-        }
-    }
-
-    let payload = [IoSlice::new(data)];
-    let msg = MsgHdr::new().with_buffers(&payload).with_control(&control);
-    socket.sendmsg(&msg, 0)?;
-    Ok(())
-}
-
-fn send_json_message<T: ?Sized + Serialize>(
-    socket: &Socket,
-    msg: &T,
-    fds: &[BorrowedFd<'_>],
-) -> std::io::Result<()> {
-    let data = serde_json::to_vec(msg)?;
-    send_message_bytes(socket, &data, fds)
-}
-
-struct EscalateSocket {
-    server: Socket,
-    client: Socket,
-}
-
-impl EscalateSocket {
-    fn open() -> anyhow::Result<EscalateSocket> {
-        let (server, client) = Socket::pair(Domain::UNIX, Type::DGRAM, None)?;
-        client.set_cloexec(false)?;
-        let socket = EscalateSocket { server, client };
-        Ok(socket)
-    }
-}
-
 #[derive(Debug, serde::Deserialize, schemars::JsonSchema)]
 pub struct ExecParams {
     pub command: String,
@@ -212,7 +96,7 @@ pub struct ExecResult {
     pub timed_out: bool,
 }
 
-fn decide_escalate(file: &str, _argv: &[String], _workdir: &str) -> EscalateAction {
+fn decide_escalate(file: &str, _argv: &[String], _workdir: &PathBuf) -> EscalateAction {
     if file == "/bin/echo" {
         EscalateAction::Escalate
     } else {
@@ -224,7 +108,7 @@ async fn super_exec_task(
     socket: Socket,
     file: String,
     argv: Vec<String>,
-    workdir: String,
+    workdir: PathBuf,
 ) -> anyhow::Result<()> {
     socket.set_nonblocking(true)?;
     let server_async_fd = AsyncFd::new(socket)?;
@@ -399,12 +283,7 @@ async fn shell_exec(params: ExecParams) -> anyhow::Result<ExecResult> {
             arg0: None,
         },
         get_platform_sandbox().unwrap_or(SandboxType::None),
-        &SandboxPolicy::WorkspaceWrite {
-            writable_roots: vec![std::env::current_dir()?.to_path_buf()],
-            network_access: false,
-            exclude_tmpdir_env_var: false,
-            exclude_slash_tmp: false,
-        },
+        &SandboxPolicy::ReadOnly,
         &PathBuf::from("/__NONEXISTENT__"), // This is ignored for ReadOnly
         &None,
         None,
@@ -500,6 +379,7 @@ enum Commands {
     ShellExec(ShellExecArgs),
 }
 
+/// Invoked inside the sandbox to (potentially) escalate permissions.
 #[derive(Parser, Debug)]
 struct EscalateArgs {
     file: String,
@@ -516,15 +396,15 @@ impl EscalateArgs {
             .parse::<i32>()
             .context("CODEX_ESCALATE_SOCKET is not a valid integer")?;
         let client = unsafe { Socket::from_raw_fd(client_fd) };
-        let message = ClientMessage::EscalateRequest {
-            file: file.clone(),
-            argv: argv.clone(),
-            workdir: std::env::current_dir()
-                .context("failed to get current directory")?
-                .to_string_lossy()
-                .to_string(),
-        };
-        send_json_message(&client, &message, &[])?;
+        send_json_message(
+            &client,
+            &ClientMessage::EscalateRequest {
+                file: file.clone(),
+                argv: argv.clone(),
+                workdir: std::env::current_dir().context("failed to get current directory")?,
+            },
+            &[],
+        )?;
         let (message, mut fds) = receive_json_message::<ServerMessage>(&client)?;
         let ServerMessage::EscalateResponse(action) = message;
         match action {
---
+++ codex-rs/exec-server/src/socket.rs
@@ -0,0 +1,131 @@
+use libc::c_uint;
+use serde::Deserialize;
+use serde::Serialize;
+use socket2::Domain;
+use socket2::MaybeUninitSlice;
+use socket2::MsgHdr;
+use socket2::MsgHdrMut;
+use socket2::Socket;
+use socket2::Type;
+use std::io::IoSlice;
+use std::mem::MaybeUninit;
+use std::os::fd::AsRawFd;
+use std::os::fd::BorrowedFd;
+use std::os::fd::FromRawFd;
+use std::os::fd::OwnedFd;
+use std::os::fd::RawFd;
+
+#[derive(Debug)]
+struct ReceivedMessage {
+    data: Vec<u8>,
+    #[allow(dead_code)]
+    fds: Vec<OwnedFd>,
+}
+
+fn assume_init(buf: &[MaybeUninit<u8>]) -> &[u8] {
+    unsafe { std::slice::from_raw_parts(buf.as_ptr().cast(), buf.len()) }
+}
+fn control_space_for_fds(count: usize) -> usize {
+    unsafe { libc::CMSG_SPACE((count * size_of::<RawFd>()) as _) as usize }
+}
+fn extract_fds(control: &mut [MaybeUninit<u8>], len: usize) -> std::io::Result<Vec<OwnedFd>> {
+    if len == 0 {
+        return Ok(Vec::new());
+    }
+    let mut fds = Vec::new();
+    let mut hdr: libc::msghdr = unsafe { std::mem::zeroed() };
+    hdr.msg_control = control.as_mut_ptr().cast();
+    hdr.msg_controllen = len as _;
+
+    let mut cmsg = unsafe { libc::CMSG_FIRSTHDR(&hdr) };
+    while !cmsg.is_null() {
+        let level = unsafe { (*cmsg).cmsg_level };
+        let ty = unsafe { (*cmsg).cmsg_type };
+        if level == libc::SOL_SOCKET && ty == libc::SCM_RIGHTS {
+            let data_ptr = unsafe { libc::CMSG_DATA(cmsg).cast::<RawFd>() };
+            let fd_count: usize = {
+                let cmsg_data_len =
+                    unsafe { (*cmsg).cmsg_len as usize } - unsafe { libc::CMSG_LEN(0) as usize };
+                cmsg_data_len / size_of::<RawFd>()
+            };
+            for i in 0..fd_count {
+                let fd = unsafe { data_ptr.add(i).read() };
+                fds.push(unsafe { OwnedFd::from_raw_fd(fd) });
+            }
+        }
+        cmsg = unsafe { libc::CMSG_NXTHDR(&hdr, cmsg) };
+    }
+    Ok(fds)
+}
+const MAX_FDS_PER_MESSAGE: usize = 16;
+const MAX_MESSAGE_SIZE: usize = 64 * 1024;
+fn receive_message(socket: &Socket) -> std::io::Result<ReceivedMessage> {
+    let mut data = [MaybeUninit::<u8>::uninit(); MAX_MESSAGE_SIZE];
+    let mut control = vec![MaybeUninit::<u8>::uninit(); control_space_for_fds(MAX_FDS_PER_MESSAGE)];
+    let (received, control_len) = {
+        let mut bufs = [MaybeUninitSlice::new(&mut data)];
+        let mut msg = MsgHdrMut::new()
+            .with_buffers(&mut bufs)
+            .with_control(&mut control);
+        let received = socket.recvmsg(&mut msg, 0)?;
+        (received, msg.control_len())
+    };
+
+    let message = assume_init(&data[..received]).to_vec();
+    let fds = extract_fds(&mut control, control_len)?;
+    Ok(ReceivedMessage { data: message, fds })
+}
+pub(crate) fn receive_json_message<T: for<'de> Deserialize<'de>>(
+    socket: &Socket,
+) -> std::io::Result<(T, Vec<OwnedFd>)> {
+    let ReceivedMessage { data, fds } = receive_message(socket)?;
+    let message: T = serde_json::from_slice(&data)?;
+    Ok((message, fds))
+}
+fn send_message_bytes(socket: &Socket, data: &[u8], fds: &[BorrowedFd<'_>]) -> std::io::Result<()> {
+    if fds.len() > MAX_FDS_PER_MESSAGE {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            format!("too many fds: {}", fds.len()),
+        ));
+    }
+    let mut control = vec![0u8; control_space_for_fds(fds.len())];
+    unsafe {
+        let cmsg = control.as_mut_ptr().cast::<libc::cmsghdr>();
+        (*cmsg).cmsg_len = libc::CMSG_LEN(size_of::<RawFd>() as c_uint * fds.len() as c_uint) as _;
+        (*cmsg).cmsg_level = libc::SOL_SOCKET;
+        (*cmsg).cmsg_type = libc::SCM_RIGHTS;
+        let data_ptr = libc::CMSG_DATA(cmsg).cast::<RawFd>();
+        for (i, fd) in fds.iter().enumerate() {
+            data_ptr.add(i).write(fd.as_raw_fd());
+        }
+    }
+
+    let payload = [IoSlice::new(data)];
+    let msg = MsgHdr::new().with_buffers(&payload).with_control(&control);
+    socket.sendmsg(&msg, 0)?;
+    Ok(())
+}
+
+pub(crate) fn send_json_message<T: ?Sized + Serialize>(
+    socket: &Socket,
+    msg: &T,
+    fds: &[BorrowedFd<'_>],
+) -> std::io::Result<()> {
+    let data = serde_json::to_vec(msg)?;
+    send_message_bytes(socket, &data, fds)
+}
+
+pub(crate) struct EscalateSocket {
+    pub(crate) server: Socket,
+    pub(crate) client: Socket,
+}
+
+impl EscalateSocket {
+    pub(crate) fn open() -> anyhow::Result<EscalateSocket> {
+        let (server, client) = Socket::pair(Domain::UNIX, Type::DGRAM, None)?;
+        client.set_cloexec(false)?;
+        let socket = EscalateSocket { server, client };
+        Ok(socket)
+    }
+}
