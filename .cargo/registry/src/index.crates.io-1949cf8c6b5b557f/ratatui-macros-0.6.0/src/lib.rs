8db8ac61ff1421a0595dfd596934c6ff99bc8f87
Maxime Savard <waxime64@hotmail.com>
2025-11-25 11:50:06 -0500

    Revert "sprint 30"
    This reverts commit 5683aa16dd5280fa18aaaf150f7c50eba961e692.

--- .cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ratatui-macros-0.6.0/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
-
-mod layout;
-mod line;
-mod row;
-mod span;
-mod text;
--- .cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sentry-0.34.0/tests/test_async.rs
+++
@@ -1,5 +0,0 @@
-// See https://github.com/getsentry/sentry-rust/issues/184
-#[tokio::test]
-async fn test_nested_async_runtimes() {
-    let _guard = sentry::init("https://public@example.com/42");
-}
