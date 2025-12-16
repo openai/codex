8265577a1a8bd3645a86d7e140c1a08d38972308
jif-oai <jif@openai.com>
2025-12-16 12:47:49 +0000

    Better assertion

--- codex-rs/utils/pty/src/win/psuedocon.rs
+++ codex-rs/utils/pty/src/win/psuedocon.rs
@@ -353,9 +353,13 @@ fn append_quoted(arg: &OsStr, cmdline: &mut Vec<u16>) {
 #[cfg(test)]
 mod tests {
     use super::windows_build_number;
+    use super::MIN_CONPTY_BUILD;
 
     #[test]
     fn windows_build_number_returns_value() {
-        assert_eq!(windows_build_number().is_some(), true);
+        // We can't stably check the version of the GH workers, but we can
+        // at least check that this.
+        let version = windows_build_number().unwrap();
+        assert!(version > MIN_CONPTY_BUILD);
     }
 }
