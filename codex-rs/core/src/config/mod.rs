4e7d39851b40c98a29e75cbfd1d7f189b27b44af
tavaresgmg <tavaresgmg@gmail.com>
2026-01-15 16:19:11 -0300

    refactor(core): extract find_codex_home to codex-utils-home
    Moved find_codex_home from codex-core (and duplicated in rmcp-client) to a new shared utility crate codex-utils-home. This resolves a TODO regarding code duplication and dependency structure.

--- codex-rs/core/src/config/mod.rs
+++ codex-rs/core/src/config/mod.rs
@@ -49,7 +49,7 @@ use codex_protocol::openai_models::ReasoningEffort;
 use codex_rmcp_client::OAuthCredentialsStoreMode;
 use codex_utils_absolute_path::AbsolutePathBuf;
 use codex_utils_absolute_path::AbsolutePathBufGuard;
-use dirs::home_dir;
+
 use schemars::JsonSchema;
 use serde::Deserialize;
 use serde::Serialize;
@@ -81,6 +81,7 @@ pub use service::ConfigService;
 pub use service::ConfigServiceError;
 
 pub use codex_git::GhostSnapshotConfig;
+pub use codex_utils_home::find_codex_home;
 
 /// Maximum number of bytes of the documentation that will be embedded. Larger
 /// files are *silently truncated* to this size so we do not take up too much of
@@ -1673,32 +1674,7 @@ impl Config {
     }
 }
 
-/// Returns the path to the Codex configuration directory, which can be
-/// specified by the `CODEX_HOME` environment variable. If not set, defaults to
-/// `~/.codex`.
-///
-/// - If `CODEX_HOME` is set, the value will be canonicalized and this
-///   function will Err if the path does not exist.
-/// - If `CODEX_HOME` is not set, this function does not verify that the
-///   directory exists.
-pub fn find_codex_home() -> std::io::Result<PathBuf> {
-    // Honor the `CODEX_HOME` environment variable when it is set to allow users
-    // (and tests) to override the default location.
-    if let Ok(val) = std::env::var("CODEX_HOME")
-        && !val.is_empty()
-    {
-        return PathBuf::from(val).canonicalize();
-    }
 
-    let mut p = home_dir().ok_or_else(|| {
-        std::io::Error::new(
-            std::io::ErrorKind::NotFound,
-            "Could not find home directory",
-        )
-    })?;
-    p.push(".codex");
-    Ok(p)
-}
 
 /// Returns the path to the folder where Codex logs are stored. Does not verify
 /// that the directory exists.
--- codex-rs/utils/home/src/lib.rs
+++ codex-rs/utils/home/src/lib.rs
@@ -1,10 +1,6 @@
 use dirs::home_dir;
 use std::path::PathBuf;
 
-/// This was copied from codex-core but codex-core depends on this crate.
-/// TODO: move this to a shared crate lower in the dependency tree.
-///
-///
 /// Returns the path to the Codex configuration directory, which can be
 /// specified by the `CODEX_HOME` environment variable. If not set, defaults to
 /// `~/.codex`.
@@ -13,7 +9,7 @@ use std::path::PathBuf;
 ///   function will Err if the path does not exist.
 /// - If `CODEX_HOME` is not set, this function does not verify that the
 ///   directory exists.
-pub(crate) fn find_codex_home() -> std::io::Result<PathBuf> {
+pub fn find_codex_home() -> std::io::Result<PathBuf> {
     // Honor the `CODEX_HOME` environment variable when it is set to allow users
     // (and tests) to override the default location.
     if let Ok(val) = std::env::var("CODEX_HOME")
