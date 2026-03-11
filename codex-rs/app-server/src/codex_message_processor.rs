b9e700fc1a0995cc9733366132d000877d696677
Sayan Sisodiya <sayan@openai.com>
2026-03-11 16:29:57 -0700

    use AVAILABLE and ON_INSTALL as default plugin install and auth policies

--- codex-rs/app-server/src/codex_message_processor.rs
+++ codex-rs/app-server/src/codex_message_processor.rs
@@ -5400,8 +5400,8 @@ impl CodexMessageProcessor {
                                         PluginSource::Local { path }
                                     }
                                 },
-                                install_policy: plugin.install_policy.map(Into::into),
-                                auth_policy: plugin.auth_policy.map(Into::into),
+                                install_policy: plugin.install_policy.into(),
+                                auth_policy: plugin.auth_policy.into(),
                                 interface: plugin.interface.map(|interface| PluginInterface {
                                     display_name: interface.display_name,
                                     short_description: interface.short_description,
@@ -5654,7 +5654,7 @@ impl CodexMessageProcessor {
                     .send_response(
                         request_id,
                         PluginInstallResponse {
-                            auth_policy: result.auth_policy.map(Into::into),
+                            auth_policy: result.auth_policy.into(),
                             apps_needing_auth,
                         },
                     )
