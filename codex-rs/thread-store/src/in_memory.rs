johnzfitch/codexabc

wip paginated apis + thread store interface changes

codex-rs/thread-store/src/in_memory.rs
@@ -28,11 +28,67 @@ use crate::ThreadStoreError;
 use crate::ThreadStoreResult;
 use crate::UpdateThreadMetadataParams;
 
+#[cfg(test)]
+use crate::ListThreadTurnItemsParams;
+#[cfg(test)]
+use crate::ListThreadTurnsParams;
+#[cfg(test)]
+use crate::SortDirection;
+#[cfg(test)]
+use crate::StoredTurnItemsView;
+
 static IN_MEMORY_THREAD_STORES: OnceLock<Mutex<HashMap<String, Arc<InMemoryThreadStore>>>> =
     OnceLock::new();
 
 fn stores() -> &'static Mutex<HashMap<String, Arc<InMemoryThreadStore>>> {
     IN_MEMORY_THREAD_STORES.get_or_init(|| Mutex::new(HashMap::new()))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn default_turn_pagination_methods_return_unsupported() {
+        let store = InMemoryThreadStore::default();
+        let thread_id = ThreadId::default();
+
+        let turns_err = store
+            .list_thread_turns(ListThreadTurnsParams {
+                thread_id,
+                include_archived: true,
+                cursor: None,
+                page_size: 10,
+                sort_direction: SortDirection::Asc,
+                items_view: StoredTurnItemsView::Summary,
+            })
+            .await
+            .expect_err("default list_thread_turns should be unsupported");
+        assert!(matches!(
+            turns_err,
+            ThreadStoreError::Unsupported {
+                operation: "list_thread_turns"
+            }
+        ));
+
+        let items_err = store
+            .list_thread_turn_items(ListThreadTurnItemsParams {
+                thread_id,
+                turn_id: "turn_1".to_string(),
+                include_archived: true,
+                cursor: None,
+                page_size: 10,
+                sort_direction: SortDirection::Asc,
+            })
+            .await
+            .expect_err("default list_thread_turn_items should be unsupported");
+        assert!(matches!(
+            items_err,
+            ThreadStoreError::Unsupported {
+                operation: "list_thread_turn_items"
+            }
+        ));
+    }
 }
 
 fn stores_guard() -> MutexGuard<'static, HashMap<String, Arc<InMemoryThreadStore>>> {

codex-rs/thread-store/src/lib.rs
@@ -26,6 +26,8 @@ pub use types::AppendThreadItemsParams;
 pub use types::ArchiveThreadParams;
 pub use types::CreateThreadParams;
 pub use types::GitInfoPatch;
+pub use types::ListThreadTurnItemsParams;
+pub use types::ListThreadTurnsParams;
 pub use types::ListThreadsParams;
 pub use types::LoadThreadHistoryParams;
 pub use types::OptionalStringPatch;
@@ -35,9 +37,13 @@ pub use types::ResumeThreadParams;
 pub use types::SortDirection;
 pub use types::StoredThread;
 pub use types::StoredThreadHistory;
+pub use types::StoredThreadTurn;
+pub use types::StoredTurnItemsView;
 pub use types::ThreadEventPersistenceMode;
 pub use types::ThreadMetadataPatch;
 pub use types::ThreadPage;
 pub use types::ThreadPersistenceMetadata;
 pub use types::ThreadSortKey;
+pub use types::ThreadTurnItemsPage;
+pub use types::ThreadTurnPage;
 pub use types::UpdateThreadMetadataParams;

openai/codex

codex-rs/thread-store/src/in_memory.rs
@@ -35,6 +35,57 @@ fn stores() -> &'static Mutex<HashMap<String, Arc<InMemoryThreadStore>>> {
     IN_MEMORY_THREAD_STORES.get_or_init(|| Mutex::new(HashMap::new()))
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ListItemsParams;
+    use crate::ListTurnsParams;
+    use crate::SortDirection;
+    use crate::StoredTurnItemsView;
+
+    #[tokio::test]
+    async fn default_turn_pagination_methods_return_unsupported() {
+        let store = InMemoryThreadStore::default();
+        let thread_id = ThreadId::default();
+
+        let turns_err = store
+            .list_turns(ListTurnsParams {
+                thread_id,
+                include_archived: true,
+                cursor: None,
+                page_size: 10,
+                sort_direction: SortDirection::Asc,
+                items_view: StoredTurnItemsView::Summary,
+            })
+            .await
+            .expect_err("default list_turns should be unsupported");
+        assert!(matches!(
+            turns_err,
+            ThreadStoreError::Unsupported {
+                operation: "list_turns"
+            }
+        ));
+
+        let items_err = store
+            .list_items(ListItemsParams {
+                thread_id,
+                turn_id: "turn_1".to_string(),
+                include_archived: true,
+                cursor: None,
+                page_size: 10,
+                sort_direction: SortDirection::Asc,
+            })
+            .await
+            .expect_err("default list_items should be unsupported");
+        assert!(matches!(
+            items_err,
+            ThreadStoreError::Unsupported {
+                operation: "list_items"
+            }
+        ));
+    }
+}
+
 fn stores_guard() -> MutexGuard<'static, HashMap<String, Arc<InMemoryThreadStore>>> {
     match stores().lock() {
         Ok(guard) => guard,

codex-rs/thread-store/src/lib.rs
@@ -26,7 +26,10 @@ pub use types::AppendThreadItemsParams;
 pub use types::ArchiveThreadParams;
 pub use types::CreateThreadParams;
 pub use types::GitInfoPatch;
+pub use types::ItemPage;
+pub use types::ListItemsParams;
 pub use types::ListThreadsParams;
+pub use types::ListTurnsParams;
 pub use types::LoadThreadHistoryParams;
 pub use types::OptionalStringPatch;
 pub use types::ReadThreadByRolloutPathParams;
@@ -35,9 +38,14 @@ pub use types::ResumeThreadParams;
 pub use types::SortDirection;
 pub use types::StoredThread;
 pub use types::StoredThreadHistory;
+pub use types::StoredTurn;
+pub use types::StoredTurnError;
+pub use types::StoredTurnItemsView;
+pub use types::StoredTurnStatus;
 pub use types::ThreadEventPersistenceMode;
 pub use types::ThreadMetadataPatch;
 pub use types::ThreadPage;
 pub use types::ThreadPersistenceMetadata;
 pub use types::ThreadSortKey;
+pub use types::TurnPage;
 pub use types::UpdateThreadMetadataParams;