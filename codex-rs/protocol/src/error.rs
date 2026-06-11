098a47fd3627b1fc1b0741eed4d3098df984bd6a
Katia Bazzi <kbazzi@openai.com>
2026-06-10 21:04:24 -0700

    Handle Guardian infrastructure failures separately

--- codex-rs/protocol/src/error.rs
+++ codex-rs/protocol/src/error.rs
@@ -228,6 +228,9 @@ impl CodexErr {
             CodexErr::RetryLimit(_) => CodexErrorInfo::ResponseTooManyFailedAttempts {
                 http_status_code: self.http_status_code_value(),
             },
+            CodexErr::Stream(..) => CodexErrorInfo::ResponseStreamDisconnected {
+                http_status_code: self.http_status_code_value(),
+            },
             CodexErr::ConnectionFailed(_) => CodexErrorInfo::HttpConnectionFailed {
                 http_status_code: self.http_status_code_value(),
             },
