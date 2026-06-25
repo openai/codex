goodalexander/OneRing

[config] Activate managed-layer precedence

codex-rs/config/src/cloud_config_bundle.rs
@@ -5,11 +5,14 @@
 //! layer entries while preserving each bucket's insertion semantics.
 
 use crate::CloudConfigFragment;
+use crate::CloudManagedLayer;
 use crate::ConfigLayerEntry;
 use crate::RequirementSource;
 use crate::RequirementsLayerEntry;
 use crate::cloud_config_layers::CloudConfigLayerError;
 use crate::cloud_config_layers::cloud_config_layers_from_fragments_strict;
+use crate::cloud_config_layers::cloud_managed_config_layers_from_fragments;
+use crate::cloud_config_layers::cloud_managed_config_layers_from_fragments_strict;
 use crate::cloud_config_layers_from_fragments;
 use codex_utils_absolute_path::AbsolutePathBuf;
 use futures::future::BoxFuture;
@@ -46,15 +49,6 @@ impl CloudConfigBundle {
             && config_managed_layers.is_absent()
             && requirements_enterprise_managed.is_empty()
             && requirements_managed_layers.is_absent()
-    }
-
-    /// Returns true when any managed bucket was present in the backend payload.
-    ///
-    /// An explicitly empty bucket still counts as present because it can suppress
-    /// compatibility fallback behavior when the bundle is materialized.
-    pub fn has_managed_layer_buckets(&self) -> bool {
-        !self.config_toml.managed_layers.is_absent()
-            || !self.requirements_toml.managed_layers.is_absent()
     }
 }
 
@@ -183,18 +177,46 @@ impl CloudConfigBundleLayers {
                 cloud_config_layers_from_fragments(fragments, base_dir)
             }
         };
-        let baseline_config = config_baseline.map(parse_config_fragments).transpose()?;
+        let parse_managed_config_fragments = |fragments, layer| {
+            if strict_config {
+                cloud_managed_config_layers_from_fragments_strict(fragments, base_dir, layer)
+            } else {
+                cloud_managed_config_layers_from_fragments(fragments, base_dir, layer)
+            }
+        };
+        let baseline_config = config_baseline
+            .map(|fragments| parse_managed_config_fragments(fragments, CloudManagedLayer::Baseline))
+            .transpose()?;
         let system_overlay_config = config_system_overlay
-            .map(parse_config_fragments)
+            .map(|fragments| {
+                parse_managed_config_fragments(fragments, CloudManagedLayer::SystemOverlay)
+            })
             .transpose()?;
         let enterprise_managed_config = parse_config_fragments(config_enterprise_managed)?;
 
-        let baseline_requirements = requirements_baseline
-            .map(|fragments| requirements_layers_from_fragments(fragments, base_dir));
-        let system_overlay_requirements = requirements_system_overlay
-            .map(|fragments| requirements_layers_from_fragments(fragments, base_dir));
-        let enterprise_managed_requirements =
-            requirements_layers_from_fragments(requirements_enterprise_managed, base_dir);
+        let baseline_requirements = requirements_baseline.map(|fragments| {
+            requirements_layers_from_fragments(fragments, base_dir, |id, name| {
+                RequirementSource::CloudManaged {
+                    layer: CloudManagedLayer::Baseline,
+                    id,
+                    name,
+                }
+            })
+        });
+        let system_overlay_requirements = requirements_system_overlay.map(|fragments| {
+            requirements_layers_from_fragments(fragments, base_dir, |id, name| {
+                RequirementSource::CloudManaged {
+                    layer: CloudManagedLayer::SystemOverlay,
+                    id,
+                    name,
+                }
+            })
+        });
+        let enterprise_managed_requirements = requirements_layers_from_fragments(
+            requirements_enterprise_managed,
+            base_dir,
+            |id, name| RequirementSource::EnterpriseManaged { id, name },
+        );
 
         Ok(Self {
             baseline_config,
@@ -210,15 +232,13 @@ impl CloudConfigBundleLayers {
 fn requirements_layers_from_fragments(
     fragments: Vec<CloudRequirementsFragment>,
     base_dir: &AbsolutePathBuf,
+    source_for_fragment: impl Fn(String, String) -> RequirementSource,
 ) -> Vec<RequirementsLayerEntry> {
     let mut layers = fragments
         .into_iter()
         .map(|fragment| {
             RequirementsLayerEntry::from_toml(
-                RequirementSource::EnterpriseManaged {
-                    id: fragment.id,
-                    name: fragment.name,
-                },
+                source_for_fragment(fragment.id, fragment.name),
                 fragment.contents,
             )
             .with_base_dir(base_dir.clone())

codex-rs/config/src/lib.rs
@@ -46,6 +46,7 @@ pub use cloud_config_layers::CloudConfigFragment;
 pub use cloud_config_layers::CloudConfigFragmentSource;
 pub use cloud_config_layers::CloudConfigLayerError;
 pub use cloud_config_layers::cloud_config_layers_from_fragments;
+pub use codex_app_server_protocol::CloudManagedLayer;
 pub use codex_app_server_protocol::ConfigLayerSource;
 pub use codex_protocol::config_types::ProfileV2Name;
 pub use codex_protocol::config_types::ProfileV2NameParseError;

codex-rs/tui/src/debug_config.rs
@@ -309,6 +309,7 @@ fn render_non_file_layer_details(layer: &ConfigLayerEntry) -> Vec<Line<'static>>
         ConfigLayerSource::SessionFlags => render_session_flag_details(&layer.config),
         ConfigLayerSource::Mdm { .. }
         | ConfigLayerSource::EnterpriseManaged { .. }
+        | ConfigLayerSource::CloudManaged { .. }
         | ConfigLayerSource::LegacyManagedConfigTomlFromMdm => render_non_file_layer_value(layer),
         ConfigLayerSource::System { .. }
         | ConfigLayerSource::User { .. }
@@ -373,6 +374,7 @@ fn non_file_layer_value_label(source: &ConfigLayerSource) -> &'static str {
             "MDM value"
         }
         ConfigLayerSource::EnterpriseManaged { .. } => "Enterprise-managed config value",
+        ConfigLayerSource::CloudManaged { .. } => "Cloud-managed config value",
         ConfigLayerSource::SessionFlags
         | ConfigLayerSource::System { .. }
         | ConfigLayerSource::User { .. }
@@ -557,6 +559,7 @@ mod tests {
     use super::session_all_proxy_url;
     use crate::legacy_core::config::Permissions;
     use codex_app_server_protocol::AskForApproval;
+    use codex_app_server_protocol::CloudManagedLayer;
     use codex_app_server_protocol::ConfigLayerSource;
     use codex_config::ConfigLayerEntry;
     use codex_config::ConfigLayerStack;
@@ -798,13 +801,23 @@ mod tests {
             absolute_path("/home/alice/.codex/config.toml")
         };
         let stack = ConfigLayerStack::new(
-            vec![ConfigLayerEntry::new(
-                ConfigLayerSource::User {
-                    file: user_file,
-                    profile: None,
-                },
-                empty_toml_table(),
-            )],
+            vec![
+                ConfigLayerEntry::new(
+                    ConfigLayerSource::CloudManaged {
+                        layer: CloudManagedLayer::Baseline,
+                        id: "policy-1".to_string(),
+                        name: "Baseline policy".to_string(),
+                    },
+                    TomlValue::Table(toml::toml! { model = "managed" }),
+                ),
+                ConfigLayerEntry::new(
+                    ConfigLayerSource::User {
+                        file: user_file,
+                        profile: None,
+                    },
+                    empty_toml_table(),
+                ),
+            ],
             requirements,
             requirements_toml,
         )

openai/codex

codex-rs/config/src/cloud_config_bundle.rs
@@ -5,12 +5,13 @@
 //! layer entries while preserving each bucket's insertion semantics.
 
 use crate::CloudConfigFragment;
+use crate::CloudManagedLayer;
 use crate::ConfigLayerEntry;
 use crate::RequirementSource;
 use crate::RequirementsLayerEntry;
 use crate::cloud_config_layers::CloudConfigLayerError;
-use crate::cloud_config_layers::cloud_config_layers_from_fragments_strict;
-use crate::cloud_config_layers_from_fragments;
+use crate::cloud_config_layers::cloud_managed_config_layers_from_fragments;
+use crate::cloud_config_layers::cloud_managed_config_layers_from_fragments_strict;
 use codex_utils_absolute_path::AbsolutePathBuf;
 use futures::future::BoxFuture;
 use futures::future::FutureExt;
@@ -131,20 +132,30 @@ impl CloudConfigBundleLayers {
                 },
         } = bundle;
 
-        let parse_config_fragments = |fragments| {
+        let parse_managed_config_fragments = |fragments, layer| {
             if strict_config {
-                cloud_config_layers_from_fragments_strict(fragments, base_dir)
+                cloud_managed_config_layers_from_fragments_strict(fragments, base_dir, layer)
             } else {
-                cloud_config_layers_from_fragments(fragments, base_dir)
+                cloud_managed_config_layers_from_fragments(fragments, base_dir, layer)
             }
         };
-        let baseline_config = parse_config_fragments(config_baseline)?;
-        let system_overlay_config = parse_config_fragments(config_system_overlay)?;
+        let baseline_config =
+            parse_managed_config_fragments(config_baseline, CloudManagedLayer::Baseline)?;
+        let system_overlay_config = parse_managed_config_fragments(
+            config_system_overlay,
+            CloudManagedLayer::SystemOverlay,
+        )?;
 
-        let baseline_requirements =
-            requirements_layers_from_fragments(requirements_baseline, base_dir);
-        let system_overlay_requirements =
-            requirements_layers_from_fragments(requirements_system_overlay, base_dir);
+        let baseline_requirements = requirements_layers_from_fragments(
+            requirements_baseline,
+            base_dir,
+            CloudManagedLayer::Baseline,
+        );
+        let system_overlay_requirements = requirements_layers_from_fragments(
+            requirements_system_overlay,
+            base_dir,
+            CloudManagedLayer::SystemOverlay,
+        );
 
         Ok(Self {
             baseline_config,
@@ -158,12 +169,14 @@ impl CloudConfigBundleLayers {
 fn requirements_layers_from_fragments(
     fragments: Vec<CloudRequirementsFragment>,
     base_dir: &AbsolutePathBuf,
+    layer: CloudManagedLayer,
 ) -> Vec<RequirementsLayerEntry> {
     let mut layers = fragments
         .into_iter()
         .map(|fragment| {
             RequirementsLayerEntry::from_toml(
-                RequirementSource::EnterpriseManaged {
+                RequirementSource::CloudManaged {
+                    layer,
                     id: fragment.id,
                     name: fragment.name,
                 },

codex-rs/config/src/lib.rs
@@ -49,6 +49,7 @@ pub use cloud_config_layers::cloud_config_layers_from_fragments;
 pub use codex_protocol::config_types::ProfileV2Name;
 pub use codex_protocol::config_types::ProfileV2NameParseError;
 pub use codex_utils_absolute_path::AbsolutePathBuf;
+pub use config_layer_source::CloudManagedLayer;
 pub use config_layer_source::ConfigLayer;
 pub use config_layer_source::ConfigLayerMetadata;
 pub use config_layer_source::ConfigLayerSource;

codex-rs/tui/src/debug_config.rs
@@ -309,6 +309,7 @@ fn render_non_file_layer_details(layer: &ConfigLayerEntry) -> Vec<Line<'static>>
         ConfigLayerSource::SessionFlags => render_session_flag_details(&layer.config),
         ConfigLayerSource::Mdm { .. }
         | ConfigLayerSource::EnterpriseManaged { .. }
+        | ConfigLayerSource::CloudManaged { .. }
         | ConfigLayerSource::LegacyManagedConfigTomlFromMdm => render_non_file_layer_value(layer),
         ConfigLayerSource::System { .. }
         | ConfigLayerSource::User { .. }
@@ -373,6 +374,7 @@ fn non_file_layer_value_label(source: &ConfigLayerSource) -> &'static str {
             "MDM value"
         }
         ConfigLayerSource::EnterpriseManaged { .. } => "Enterprise-managed config value",
+        ConfigLayerSource::CloudManaged { .. } => "Cloud-managed config value",
         ConfigLayerSource::SessionFlags
         | ConfigLayerSource::System { .. }
         | ConfigLayerSource::User { .. }
@@ -557,6 +559,7 @@ mod tests {
     use super::session_all_proxy_url;
     use crate::legacy_core::config::Permissions;
     use codex_app_server_protocol::AskForApproval;
+    use codex_config::CloudManagedLayer;
     use codex_config::ConfigLayerEntry;
     use codex_config::ConfigLayerSource;
     use codex_config::ConfigLayerStack;
@@ -799,13 +802,23 @@ mod tests {
             absolute_path("/home/alice/.codex/config.toml")
         };
         let stack = ConfigLayerStack::new(
-            vec![ConfigLayerEntry::new(
-                ConfigLayerSource::User {
-                    file: user_file,
-                    profile: None,
-                },
-                empty_toml_table(),
-            )],
+            vec![
+                ConfigLayerEntry::new(
+                    ConfigLayerSource::CloudManaged {
+                        layer: CloudManagedLayer::Baseline,
+                        id: "policy-1".to_string(),
+                        name: "Baseline policy".to_string(),
+                    },
+                    TomlValue::Table(toml::toml! { model = "managed" }),
+                ),
+                ConfigLayerEntry::new(
+                    ConfigLayerSource::User {
+                        file: user_file,
+                        profile: None,
+                    },
+                    empty_toml_table(),
+                ),
+            ],
             requirements,
             requirements_toml,
         )