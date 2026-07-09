suqi8/naicode

[tools] Restrict hosted threads to server-registered tools

codex-rs/config/src/merge_tests.rs
@@ -2,6 +2,8 @@ use super::*;
 use crate::config_toml::ConfigToml;
 use crate::types::MemoriesToml;
 use pretty_assertions::assert_eq;
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
 
 fn parse_toml(value: &str) -> TomlValue {
     toml::from_str(value).expect("TOML should parse")
@@ -123,4 +125,41 @@ fn merge_toml_values_normalizes_permission_network_domains_before_overlaying() {
 "#,
     );
     assert_eq!(base, expected);
+}
+
+#[test]
+fn server_registered_mcp_tool_policy_replaces_lower_layer_policy() {
+    let mut base = parse_toml(
+        r#"
+[features.server_registered_tools_only]
+enabled = true
+mcp_tools = [
+    { server_name = "user_server", tool_name = "write" },
+]
+"#,
+    );
+    let overlay = parse_toml(
+        r#"
+[features.server_registered_tools_only]
+enabled = true
+mcp_tools = [
+    { server_name = "hoopa", tool_name = "list_addresses" },
+    { server_name = "hoopa", tool_name = "read_message" },
+]
+"#,
+    );
+
+    merge_toml_values(&mut base, &overlay);
+
+    let config: ConfigToml = base.try_into().expect("merged config should deserialize");
+    assert_eq!(
+        config
+            .features
+            .expect("features should be present")
+            .server_registered_mcp_tools(),
+        BTreeMap::from([(
+            "hoopa".to_string(),
+            BTreeSet::from(["list_addresses".to_string(), "read_message".to_string()]),
+        )])
+    );
 }

codex-rs/core/src/tools/spec_plan_tests.rs
@@ -1,4 +1,5 @@
 use std::collections::BTreeMap;
+use std::collections::BTreeSet;
 use std::collections::HashMap;
 use std::sync::Arc;
 
@@ -916,6 +917,86 @@ async fn invalid_mcp_tools_are_not_registered() {
 
     plan.assert_visible_lacks(&["mcp__invalid"]);
     plan.assert_registered_lacks(&[&ToolName::namespaced("mcp__invalid", "lookup").to_string()]);
+}
+
+#[tokio::test]
+async fn server_registered_tools_only_restricts_the_complete_tool_inventory() {
+    let plan = probe_with(
+        |turn| {
+            set_features(
+                turn,
+                &[
+                    Feature::Apps,
+                    Feature::ServerRegisteredToolsOnly,
+                    Feature::CodeMode,
+                    Feature::CodeModeOnly,
+                    Feature::ImageGeneration,
+                    Feature::MultiAgentV2,
+                    Feature::Plugins,
+                    Feature::RequestPermissionsTool,
+                    Feature::ShellTool,
+                    Feature::ToolSuggest,
+                ],
+            );
+            set_web_search_mode(turn, WebSearchMode::Live);
+            turn.model_info.supports_search_tool = true;
+            turn.model_info.web_search_tool_type = WebSearchToolType::TextAndImage;
+            turn.model_info.apply_patch_tool_type = Some(ApplyPatchToolType::Freeform);
+            turn.model_info.input_modalities = vec![InputModality::Image];
+            update_config(turn, |config| {
+                config.server_registered_mcp_tools = BTreeMap::from([(
+                    "hoopa".to_string(),
+                    BTreeSet::from(["list_addresses".to_string(), "read_message".to_string()]),
+                )]);
+            });
+            use_actor_authorized_provider(turn);
+        },
+        ToolPlanInputs {
+            mcp_tools: Some(vec![
+                mcp_tool("hoopa", "mcp__hoopa", "list_addresses"),
+                mcp_tool("hoopa", "mcp__hoopa", "read_message"),
+                mcp_tool("hoopa", "mcp__hoopa", "send_message"),
+                mcp_tool("user_server", "mcp__user_server", "lookup"),
+            ]),
+            deferred_mcp_tools: Some(vec![mcp_tool(
+                "user_deferred",
+                "mcp__user_deferred",
+                "lookup",
+            )]),
+            tool_suggest_candidates: Some(plugin_candidates(ToolSuggestPresentation::ListTool)),
+            extension_tool_executors: vec![Arc::new(DeferredExtensionTool)],
+            dynamic_tools: vec![
+                dynamic_tool(/*namespace*/ None, "server_callback", false),
+                dynamic_tool(/*namespace*/ None, "server_deferred", true),
+            ],
+        },
+    )
+    .await;
+
+    assert_eq!(plan.visible_names, vec!["mcp__hoopa", "server_callback"]);
+    assert_eq!(
+        plan.namespace_function_names("mcp__hoopa"),
+        &["list_addresses".to_string(), "read_message".to_string()]
+    );
+    assert_eq!(
+        plan.registered_names,
+        vec![
+            ToolName::namespaced("mcp__hoopa", "list_addresses").to_string(),
+            ToolName::namespaced("mcp__hoopa", "read_message").to_string(),
+            "server_callback".to_string(),
+            "server_deferred".to_string(),
+        ]
+    );
+    assert_eq!(
+        plan.exposure(&ToolName::namespaced("mcp__hoopa", "list_addresses").to_string()),
+        ToolExposure::Direct
+    );
+    assert_eq!(
+        plan.exposure(&ToolName::namespaced("mcp__hoopa", "read_message").to_string()),
+        ToolExposure::Direct
+    );
+    assert_eq!(plan.exposure("server_callback"), ToolExposure::Direct);
+    assert_eq!(plan.exposure("server_deferred"), ToolExposure::Deferred);
 }
 
 #[tokio::test]

codex-rs/features/src/tests.rs
@@ -11,6 +11,7 @@ use codex_protocol::protocol::EventMsg;
 use codex_protocol::protocol::WarningEvent;
 use pretty_assertions::assert_eq;
 use std::collections::BTreeMap;
+use std::collections::BTreeSet;
 use toml::Table;
 use toml::Value as TomlValue;
 
@@ -643,12 +644,44 @@ non_code_mode_only = true
 }
 
 #[test]
+fn server_registered_tools_only_deserializes_exact_raw_mcp_policy() {
+    let features: FeaturesToml = toml::from_str(
+        r#"
+[server_registered_tools_only]
+enabled = true
+mcp_tools = [
+    { server_name = "hoopa", tool_name = "list_addresses" },
+    { server_name = "hoopa", tool_name = "read_message" },
+]
+"#,
+    )
+    .expect("features table should deserialize");
+
+    assert_eq!(
+        features.entries(),
+        BTreeMap::from([("server_registered_tools_only".to_string(), true)])
+    );
+    assert_eq!(
+        features.server_registered_mcp_tools(),
+        BTreeMap::from([(
+            "hoopa".to_string(),
+            BTreeSet::from(["list_addresses".to_string(), "read_message".to_string()]),
+        )])
+    );
+
+    let boolean_only: FeaturesToml = toml::from_str("server_registered_tools_only = true")
+        .expect("boolean feature toggle should deserialize");
+    assert_eq!(boolean_only.server_registered_mcp_tools(), BTreeMap::new());
+}
+
+#[test]
 fn materialize_resolved_enabled_writes_all_features_and_preserves_custom_config() {
     let mut features = Features::with_defaults();
     features.enable(Feature::CodeMode);
     features.enable(Feature::MultiAgentV2);
     features.enable(Feature::NetworkProxy);
     features.enable(Feature::RespectSystemProxy);
+    features.enable(Feature::ServerRegisteredToolsOnly);
 
     let mut features_toml = FeaturesToml {
         multi_agent_v2: Some(FeatureToml::Config(crate::MultiAgentV2ConfigToml {
@@ -661,6 +694,15 @@ fn materialize_resolved_enabled_writes_all_features_and_preserves_custom_config(
             proxy_url: Some("http://127.0.0.1:43128".to_string()),
             ..Default::default()
         })),
+        server_registered_tools_only: Some(FeatureToml::Config(
+            crate::ServerRegisteredToolsOnlyConfigToml {
+                enabled: Some(false),
+                mcp_tools: vec![crate::ServerRegisteredMcpToolToml {
+                    server_name: "hoopa".to_string(),
+                    tool_name: "read_message".to_string(),
+                }],
+            },
+        )),
         entries: BTreeMap::new(),
         ..Default::default()
     };
@@ -691,6 +733,13 @@ fn materialize_resolved_enabled_writes_all_features_and_preserves_custom_config(
             proxy_url: Some("http://127.0.0.1:43128".to_string()),
             ..Default::default()
         }))
+    );
+    assert_eq!(
+        features_toml.server_registered_mcp_tools(),
+        BTreeMap::from([(
+            "hoopa".to_string(),
+            BTreeSet::from(["read_message".to_string()]),
+        )])
     );
     let replayed = Features::from_sources(
         FeatureConfigSource {

openai/codex

codex-rs/config/src/merge_tests.rs
@@ -2,6 +2,8 @@ use super::*;
 use crate::config_toml::ConfigToml;
 use crate::types::MemoriesToml;
 use pretty_assertions::assert_eq;
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
 
 fn parse_toml(value: &str) -> TomlValue {
     toml::from_str(value).expect("TOML should parse")
@@ -123,4 +125,41 @@ fn merge_toml_values_normalizes_permission_network_domains_before_overlaying() {
 "#,
     );
     assert_eq!(base, expected);
+}
+
+#[test]
+fn server_registered_mcp_tool_policy_replaces_lower_layer_policy() {
+    let mut base = parse_toml(
+        r#"
+[features.server_registered_tools_only]
+enabled = true
+mcp_tools = [
+    { server_name = "user_server", tool_name = "write" },
+]
+"#,
+    );
+    let overlay = parse_toml(
+        r#"
+[features.server_registered_tools_only]
+enabled = true
+mcp_tools = [
+    { server_name = "hoopa", tool_name = "lookup" },
+    { server_name = "hoopa", tool_name = "search" },
+]
+"#,
+    );
+
+    merge_toml_values(&mut base, &overlay);
+
+    let config: ConfigToml = base.try_into().expect("merged config should deserialize");
+    assert_eq!(
+        config
+            .features
+            .expect("features should be present")
+            .server_registered_mcp_tools(),
+        BTreeMap::from([(
+            "hoopa".to_string(),
+            BTreeSet::from(["lookup".to_string(), "search".to_string()]),
+        )])
+    );
 }

codex-rs/core/src/tools/spec_plan_tests.rs
@@ -1,4 +1,5 @@
 use std::collections::BTreeMap;
+use std::collections::BTreeSet;
 use std::sync::Arc;
 
 use codex_features::Feature;
@@ -897,6 +898,101 @@ async fn invalid_mcp_tools_are_not_registered() {
 
     plan.assert_visible_lacks(&["mcp__invalid"]);
     plan.assert_registered_lacks(&[&ToolName::namespaced("mcp__invalid", "lookup").to_string()]);
+}
+
+#[tokio::test]
+async fn server_registered_tools_only_restricts_the_complete_tool_inventory() {
+    let plan = probe_with(
+        |turn| {
+            set_features(
+                turn,
+                &[
+                    Feature::Apps,
+                    Feature::ServerRegisteredToolsOnly,
+                    Feature::CodeMode,
+                    Feature::CodeModeOnly,
+                    Feature::ImageGeneration,
+                    Feature::MultiAgentV2,
+                    Feature::Plugins,
+                    Feature::RequestPermissionsTool,
+                    Feature::ShellTool,
+                    Feature::ToolSuggest,
+                ],
+            );
+            set_web_search_mode(turn, WebSearchMode::Live);
+            turn.model_info.supports_search_tool = true;
+            turn.model_info.web_search_tool_type = WebSearchToolType::TextAndImage;
+            turn.model_info.apply_patch_tool_type = Some(ApplyPatchToolType::Freeform);
+            turn.model_info.input_modalities = vec![InputModality::Image];
+            update_config(turn, |config| {
+                config.server_registered_mcp_tools = BTreeMap::from([(
+                    "hoopa".to_string(),
+                    BTreeSet::from(["lookup".to_string(), "search".to_string()]),
+                )]);
+            });
+            use_chatgpt_auth(turn);
+            assert!(!super::tool_suggest_enabled(turn));
+        },
+        ToolPlanInputs {
+            mcp_tools: Some(vec![
+                mcp_tool("hoopa", "mcp__hoopa", "lookup"),
+                mcp_tool("hoopa", "mcp__hoopa", "search"),
+                mcp_tool("hoopa", "mcp__hoopa", "update"),
+                mcp_tool("user_server", "mcp__user_server", "lookup"),
+            ]),
+            deferred_mcp_tools: Some(vec![mcp_tool(
+                "user_deferred",
+                "mcp__user_deferred",
+                "lookup",
+            )]),
+            tool_suggest_candidates: Some(plugin_candidates(ToolSuggestPresentation::ListTool)),
+            extension_tool_executors: vec![
+                Arc::new(DeferredExtensionTool),
+                Arc::new(TestNamespaceExtensionTool {
+                    namespace: "image_gen",
+                    tool_name: "imagegen",
+                }),
+            ],
+            dynamic_tools: vec![
+                dynamic_tool(
+                    /*namespace*/ None,
+                    "server_callback",
+                    /*defer_loading*/ false,
+                ),
+                dynamic_tool(
+                    /*namespace*/ None,
+                    "server_deferred",
+                    /*defer_loading*/ true,
+                ),
+            ],
+        },
+    )
+    .await;
+
+    assert_eq!(plan.visible_names, vec!["mcp__hoopa", "server_callback"]);
+    assert_eq!(
+        plan.namespace_function_names("mcp__hoopa"),
+        &["lookup".to_string(), "search".to_string()]
+    );
+    assert_eq!(
+        plan.registered_names,
+        vec![
+            ToolName::namespaced("mcp__hoopa", "lookup").to_string(),
+            ToolName::namespaced("mcp__hoopa", "search").to_string(),
+            "server_callback".to_string(),
+            "server_deferred".to_string(),
+        ]
+    );
+    assert_eq!(
+        plan.exposure(&ToolName::namespaced("mcp__hoopa", "lookup").to_string()),
+        ToolExposure::Direct
+    );
+    assert_eq!(
+        plan.exposure(&ToolName::namespaced("mcp__hoopa", "search").to_string()),
+        ToolExposure::Direct
+    );
+    assert_eq!(plan.exposure("server_callback"), ToolExposure::Direct);
+    assert_eq!(plan.exposure("server_deferred"), ToolExposure::Deferred);
 }
 
 #[tokio::test]

codex-rs/features/src/tests.rs
@@ -11,6 +11,7 @@ use codex_protocol::protocol::EventMsg;
 use codex_protocol::protocol::WarningEvent;
 use pretty_assertions::assert_eq;
 use std::collections::BTreeMap;
+use std::collections::BTreeSet;
 use toml::Table;
 use toml::Value as TomlValue;
 
@@ -681,12 +682,44 @@ non_code_mode_only = true
 }
 
 #[test]
+fn server_registered_tools_only_deserializes_exact_raw_mcp_policy() {
+    let features: FeaturesToml = toml::from_str(
+        r#"
+[server_registered_tools_only]
+enabled = true
+mcp_tools = [
+    { server_name = "hoopa", tool_name = "lookup" },
+    { server_name = "hoopa", tool_name = "search" },
+]
+"#,
+    )
+    .expect("features table should deserialize");
+
+    assert_eq!(
+        features.entries(),
+        BTreeMap::from([("server_registered_tools_only".to_string(), true)])
+    );
+    assert_eq!(
+        features.server_registered_mcp_tools(),
+        BTreeMap::from([(
+            "hoopa".to_string(),
+            BTreeSet::from(["lookup".to_string(), "search".to_string()]),
+        )])
+    );
+
+    let boolean_only: FeaturesToml = toml::from_str("server_registered_tools_only = true")
+        .expect("boolean feature toggle should deserialize");
+    assert_eq!(boolean_only.server_registered_mcp_tools(), BTreeMap::new());
+}
+
+#[test]
 fn materialize_resolved_enabled_writes_all_features_and_preserves_custom_config() {
     let mut features = Features::with_defaults();
     features.enable(Feature::CodeMode);
     features.enable(Feature::MultiAgentV2);
     features.enable(Feature::NetworkProxy);
     features.enable(Feature::RespectSystemProxy);
+    features.enable(Feature::ServerRegisteredToolsOnly);
 
     let mut features_toml = FeaturesToml {
         multi_agent_v2: Some(FeatureToml::Config(crate::MultiAgentV2ConfigToml {
@@ -699,6 +732,15 @@ fn materialize_resolved_enabled_writes_all_features_and_preserves_custom_config(
             proxy_url: Some("http://127.0.0.1:43128".to_string()),
             ..Default::default()
         })),
+        server_registered_tools_only: Some(FeatureToml::Config(
+            crate::ServerRegisteredToolsOnlyConfigToml {
+                enabled: Some(false),
+                mcp_tools: vec![crate::ServerRegisteredMcpToolToml {
+                    server_name: "hoopa".to_string(),
+                    tool_name: "search".to_string(),
+                }],
+            },
+        )),
         entries: BTreeMap::new(),
         ..Default::default()
     };
@@ -729,6 +771,10 @@ fn materialize_resolved_enabled_writes_all_features_and_preserves_custom_config(
             proxy_url: Some("http://127.0.0.1:43128".to_string()),
             ..Default::default()
         }))
+    );
+    assert_eq!(
+        features_toml.server_registered_mcp_tools(),
+        BTreeMap::from([("hoopa".to_string(), BTreeSet::from(["search".to_string()]),)])
     );
     let replayed = Features::from_sources(
         FeatureConfigSource {